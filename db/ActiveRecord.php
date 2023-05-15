eed4542d1e08fefafdd1b480cf01b3334658d263
uzdevid <uzdevid@gmail.com>
2023-05-15 16:00:38 +0500

    Добавил поведение ModifyLog

--- db/ActiveRecord.php
+++ db/ActiveRecord.php
@@ -2,6 +2,22 @@
 
 namespace uzdevid\dashboard\base\db;
 
+use uzdevid\dashboard\modify\log\behaviors\ModifyLogBehavior;
+
 class ActiveRecord extends \yii\db\ActiveRecord {
-    
+    public function behaviors(): array {
+        $behaviors = parent::behaviors();
+
+        if (class_exists(ModifyLogBehavior::class)) {
+            $behaviors['ModifyLogBehavior'] = [
+                'class' => ModifyLogBehavior::class,
+                'afterInsert' => true,
+                'afterDelete' => true,
+                'afterUpdate' => true,
+                'attributes' => $this->attributes(),
+            ];
+        }
+
+        return $behaviors;
+    }
 }
